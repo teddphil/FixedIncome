f0723f9a143e2e2698d2ff43076c419540c781c4
Teddy <dadi.zhao@outlook.com>
2025-12-22 17:58:34 +0000

    Create zero_rates_bootstrap.py

---
+++ zero_rates_bootstrap.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def calculate_zero_rates_bootstrap(coupons, maturities, principal = 100):
+
+	"""
+	coupons: list, annual coupon rates
+	maturities: list, years
+	returns: array, zero rates
+	
+	Abbreviations:
+		ZR: zero rate
+		DF: discount factor
+		CF: cash flow
+		PV: present value
+	"""
+	
+	ZRs = np.zeros(len(maturities))
+	DFs = np.zeros(len(maturities))
+	
+	for i in range(len(maturities)):
+		price = principal
+		coupon_payment = coupons[i] * principal
+		
+		if i==0:
+				previous_CFs_PV = 0
+		else:
+				previous_CFs_PV = np.sum(coupon_payment * DFs[:i])
+				
+		# Price = Sum of PVs of Previous Coupons + (Final Coupon + Principal) × Current DF
+		
+		DF_current = (price - previous_CFs_PV) / (coupon_payment + principal)
+		
+		DFs[i] = DF_current
+		
+		ZRs[i] = -np.log(DF_current) / maturities[i]
+	
+	return ZRs
+	
+rates = calculate_zero_rates_bootstrap([0.02, 0.03, 0.04], [1, 2, 3])
+print(f"Zero rates: {rates}")
+	
+			
+		
